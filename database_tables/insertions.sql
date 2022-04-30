c443a6cc11ca29a7f1a0c3815b2db52bd3f04a17
Alvaro <ram.mendoza.a.98@outlook.com>
2022-04-30 13:28:31 -0500

    Add starting questionnaire

--- database_tables/insertions.sql
+++ database_tables/insertions.sql
@@ -195,13 +195,16 @@ INSERT INTO USER_TEMPLATE_ACTIVITY (template_activity_id, user_template_section_
 VALUES 
 (1, 1, 1, 1, 1);
 
-INSERT INTO QUESTIONNAIRE (title, description, total_questions)
+INSERT INTO QUESTIONNAIRE (id, title, description, total_questions)
 VALUES
-("This is a questionnaire", "this is a description", 10);
+(7, "This is a questionnaire", "this is a description", 10);
 
 INSERT INTO QUESTIONNAIRE_QUESTION (questionnaire_id, question, answer, option_1, option_2, option_3)
 VALUES
-(1, "is this a question?", "yes", "no", "maybe", "what is the question?")
+(7, "question 1?", "yes", "no", "maybe", "what is the question?");
+(7, "question 2?", "yes", "no", "maybe", "what is the question?");
+(7, "question 3?", "yes", "no", "maybe", "what is the question?");
+(7, "question 4?", "yes", "no", "maybe", "what is the question?");
 
 INSERT INTO USER_QUESTIONNAIRE (user_id, questionnaire_id, correct_answers, percentage_score, status_id)
 VALUES
