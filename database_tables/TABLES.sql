e084ea23b2762578b30a474c39f12b1daaa14724
Alvaro <ram.mendoza.a.98@outlook.com>
2022-05-19 21:55:06 -0500

    Modify company link

--- database_tables/TABLES.sql
+++ database_tables/TABLES.sql
@@ -399,8 +399,8 @@ DROP TABLE IF EXISTS COMPANY_TRACKING_LINKS;
 CREATE TABLE COMPANY_TRACKING_LINKS (
     id int(11) unsigned NOT NULL AUTO_INCREMENT,
     company_tracking_id int(11) unsigned NOT NULL,
-    description varchar(255) NOT NULL,
-    url varchar(1000) NOT NULL,
+    description text NOT NULL,
+    url text NOT NULL,
     is_active tinyint(1) NOT NULL DEFAULT '1',
     PRIMARY KEY(id),
     CONSTRAINT fk_cotl_cotr_1 FOREIGN KEY (company_tracking_id) REFERENCES COMPANY_TRACKING (id)
